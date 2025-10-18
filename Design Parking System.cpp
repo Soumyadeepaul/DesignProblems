40671a174fe1b00e3e6a83f883f9abef244b6f05
Soumyadeep Paul <92711266+Soumyadeepaul@users.noreply.github.com>
2025-10-18 10:53:00 +0530

    Create Design Parking System.cpp

---
+++ Design Parking System.cpp
@@ -0,0 +1,46 @@
+//https://leetcode.com/problems/design-parking-system/description/
+
+
+class ParkingSystem {
+private:
+    int b;
+    int m;
+    int s;
+public:
+    ParkingSystem(int big, int medium, int small) {
+        b=big;
+        m=medium;
+        s=small;
+    }
+    
+    bool addCar(int carType) {
+        if(carType==1){
+            if(b!=0){
+                b--;
+                return true;
+            }
+            return false;
+        }
+        if(carType==2){
+            if(m!=0){
+                m--;
+                return true;
+            }
+            return false;
+        }
+        if(carType==3){
+            if(s!=0){
+                s--;
+                return true;
+            }
+            return false;
+        }
+        return false;
+    }
+};
+
+/**
+ * Your ParkingSystem object will be instantiated and called as such:
+ * ParkingSystem* obj = new ParkingSystem(big, medium, small);
+ * bool param_1 = obj->addCar(carType);
+ */
